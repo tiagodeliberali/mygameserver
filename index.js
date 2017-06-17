c380da6b13c829ecac1b7283e0ce8b22e4f41599
Tiago Deliberali Santos <tiago@vinya.com.br>
2017-06-17 11:41:58 -0300

    fixed log

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-var io = require('socket.io')(process.env.PORT || 3000);
+var port = process.env.PORT || 3000;
+
+var io = require('socket.io')(port);
 var shortid = require('shortid');
 
-console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+console.log("Express server listening on port %s mode", port);
 
 var playerList = {};
 
